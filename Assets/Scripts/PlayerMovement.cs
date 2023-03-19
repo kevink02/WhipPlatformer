65355531544e7996984044a0b74c8a52b794dafa
kevink02 <kevinkubota0@outlook.com>
2023-03-19 15:59:46 -0700

    Added player movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private PlayerControls _playerControls = new PlayerControls();
+
+    private void Awake()
+    {
+    }
+}
