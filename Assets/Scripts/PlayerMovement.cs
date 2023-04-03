885d5438dcd51e070212e44f6d9946e783b2b97e
kevink02 <kevinkubota0@outlook.com>
2023-04-03 11:38:30 -0700

    Added basic player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] [Range(0, 1000)]
     private float _jumpForce;
+    [SerializeField]
+    private float _moveForce;
 
     private PlayerControls _playerControls;
     private Rigidbody2D _rigidBody2D;
@@ -18,7 +20,8 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Start()
     {
-        _playerControls.Movement.HorizontalMove.performed += cxt => MoveHorizontally(cxt);
+        _playerControls.Movement.HorizontalMove.performed += cxt => MoveHorizontally(cxt.ReadValue<float>());
+        _playerControls.Movement.HorizontalMove.canceled += cxt => MoveHorizontally(cxt.ReadValue<float>());
         _playerControls.Movement.VerticalMove.performed += _ => MoveVertically();
         _playerControls.Abilities.Attack.performed += _ => AbilityAttack();
         _playerControls.Abilities.Interact.performed += _ => AbilityInteract();
@@ -31,13 +34,12 @@ public class PlayerMovement : MonoBehaviour
     {
         _playerControls.Disable();
     }
-    private void MoveHorizontally(InputAction.CallbackContext cxt)
+    private void MoveHorizontally(float moveDirection)
     {
-        Debug.Log($"{name}: Moving {cxt.ReadValue<float>()}");
+        _rigidBody2D.velocity = _moveForce * moveDirection * Vector2.right;
     }
     private void MoveVertically()
     {
-        Debug.Log($"{name}: Jumping");
         _rigidBody2D.AddForce(_jumpForce * Vector2.up);
     }
     private void AbilityAttack()
