b075f15bdd750e1c5d57a5824ff4806ae18fb3bb
kevink02 <kevinkubota0@outlook.com>
2023-04-03 16:16:11 -0700

    Added basic game manager class

---
+++ Assets/Scripts/Game_Manager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Manager : MonoBehaviour
+{    /// <summary>
+     /// Returns an angle in degrees
+     /// </summary>
+    public static Func<Vector2, float> GetAngleFromVector2 { get; } = vector2 => Mathf.Atan2(vector2.y, vector2.x) * Mathf.Rad2Deg;
+    public static Func<float, Vector2> GetVector2FromAngle { get; } = angleInDegrees => new Vector2(Mathf.Cos(angleInDegrees * Mathf.Deg2Rad), Mathf.Sin(angleInDegrees * Mathf.Deg2Rad));
+}
