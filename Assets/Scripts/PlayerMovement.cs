f7be35ce20a519125609c3cc9c9da367b77d9eac
kevink02 <kevinkubota0@outlook.com>
2023-04-03 11:17:17 -0700

    Added logic for player input actions

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,4 +9,25 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
     }
+    private void Start()
+    {
+        _playerControls.Movement.HorizontalMove.performed += _ => MoveHorizontally(_playerControls.Movement.HorizontalMove.ReadValue<Vector2>());
+        _playerControls.Movement.VerticalMove.performed += _ => MoveVertically(_playerControls.Movement.VerticalMove.ReadValue<Vector2>());
+    }
+    private void OnEnable()
+    {
+        _playerControls.Movement.Enable();
+    }
+    private void OnDisable()
+    {
+        _playerControls.Movement.Disable();
+    }
+    private void MoveHorizontally(Vector2 movement)
+    {
+        Debug.Log($"{name}: Moving {movement}");
+    }
+    private void MoveVertically(Vector2 movement)
+    {
+        Debug.Log($"{name}: Moving {movement}");
+    }
 }
