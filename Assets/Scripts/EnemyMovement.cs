d38767f7ceb6b4c71d4e0dbaf26f64012278d374
kevink02 <kevinkubota0@outlook.com>
2023-04-03 16:21:55 -0700

    Added comments for how to implement enemy movement

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -14,5 +14,8 @@ public class EnemyMovement : MonoBehaviour
     {
         Vector2 direction = Game_Manager.GetVector2FromAngle(_detectAngle);
         Debug.DrawRay(transform.position, direction, Color.white, Game_Manager.DebugRayLifeTime);
+
+        // Move in current direction
+        // If its raycast detects the end of its current platform, switch directions (raycast detection angle will flip to match its direction as well)
     }
 }
