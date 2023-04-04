035acfed908b0ca4932015da815888e062cdad9b
kevink02 <kevinkubota0@outlook.com>
2023-04-03 19:12:44 -0700

    Started raycast detection for enemy

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -9,13 +9,25 @@ public class EnemyMovement : MonoBehaviour
     private float _detectAngle; // default = 315
     [SerializeField]
     private float _moveSpeed;
+    private Vector2 _detectVector; // the vector corresponding to the detect angle
 
+    private void Awake()
+    {
+        _detectVector = Game_Manager.GetVector2FromAngle(_detectAngle);
+    }
     private void FixedUpdate()
     {
-        Vector2 direction = Game_Manager.GetVector2FromAngle(_detectAngle);
-        Debug.DrawRay(transform.position, direction, Color.white, Game_Manager.DebugRayLifeTime);
+        
+        Debug.DrawRay(transform.position, _detectVector, Color.white, Game_Manager.DebugRayLifeTime);
 
         // Move in current direction
         // If its raycast detects the end of its current platform, switch directions (raycast detection angle will flip to match its direction as well)
+        Ray ray = new Ray(transform.position, _detectVector);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, 100f, Game_Manager.PlatformMask);
+        // Did not detect a platform in front of it
+        if (!(hit && hit.collider.CompareTag("Platform")))
+        {
+
+        }
     }
 }
--- Assets/Scripts/Game_Manager.cs
+++ Assets/Scripts/Game_Manager.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 
 public class Game_Manager : MonoBehaviour
 {
+    [SerializeField]
+    private LayerMask _platformMask;
+    public static LayerMask PlatformMask;
+
     public static readonly float DebugRayLifeTime = 0.02f;
     /// <summary>
      /// Returns an angle in degrees
      /// </summary>
     public static Func<Vector2, float> GetAngleFromVector2 { get; } = vector2 => Mathf.Atan2(vector2.y, vector2.x) * Mathf.Rad2Deg;
     public static Func<float, Vector2> GetVector2FromAngle { get; } = angleInDegrees => new Vector2(Mathf.Cos(angleInDegrees * Mathf.Deg2Rad), Mathf.Sin(angleInDegrees * Mathf.Deg2Rad));
+
+    private void Awake()
+    {
+        PlatformMask = _platformMask;
+    }
 }
