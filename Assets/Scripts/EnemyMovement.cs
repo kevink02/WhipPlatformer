c8a0d6c2bd1b7ad823444607bdce7e49c6b85c86
kevink02 <kevinkubota0@outlook.com>
2023-04-03 19:56:53 -0700

    Implemented basic function to flip enemy movement

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -7,17 +7,30 @@ public class EnemyMovement : MonoBehaviour
     [Range(0, 360f)]
     [SerializeField]
     private float _detectAngle; // default = 315
+    [Range(0.01f, 1)]
     [SerializeField]
-    private float _moveSpeed;
+    private float _moveAccel; // default = 0.2f
+    [Range(0.01f, 1)]
+    [SerializeField]
+    private float _moveDecel; // default = 0.1f
+    [SerializeField]
+    private float _moveForce; // default = 5
+
+    private Rigidbody2D _rigidBody2D;
     private Vector2 _detectVector; // the vector corresponding to the detect angle
+    private Vector2 _moveDirection;
 
     private void Awake()
     {
+        _rigidBody2D = GetComponent<Rigidbody2D>();
         _detectVector = Game_Manager.GetVector2FromAngle(_detectAngle);
+        _moveDirection = Vector2.right;
     }
     private void FixedUpdate()
     {
-        
+        _rigidBody2D.velocity = Vector2.Lerp(_rigidBody2D.velocity, _moveForce * _moveDirection, _moveAccel);
+
+
         Debug.DrawRay(transform.position, _detectVector, Color.white, Game_Manager.DebugRayLifeTime);
 
         // Move in current direction
@@ -27,7 +40,13 @@ public class EnemyMovement : MonoBehaviour
         // Did not detect a platform in front of it
         if (!(hit && hit.collider.CompareTag("Platform")))
         {
-
+            FlipMoveDirection();
         }
+    }
+    private void FlipMoveDirection()
+    {
+        _detectVector = Vector2.Reflect(_detectVector, Vector2.down);
+        _detectAngle = Game_Manager.GetAngleFromVector2(_detectVector);
+        _moveDirection *= -1;
     }
 }
