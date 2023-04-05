120558f2393edac604c31d1cc74612736a093b8b
kevink02 <kevinkubota0@outlook.com>
2023-04-05 11:01:33 -0700

    Added camera ability to zoom in and out

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -7,24 +7,28 @@ public class CameraMovement : MonoBehaviour
     [Range(0.01f, 1)]
     [SerializeField]
     private float _lerpSpeed;
+    private Camera _camera;
     private PlayerMovement _player;
 
     private static bool _isZoomedOut;
 
     private void Awake()
     {
+        _camera = GetComponent<Camera>();
         _player = FindObjectOfType<PlayerMovement>();
     }
     private void FixedUpdate()
     {
         if (_isZoomedOut)
         {
-
+            transform.position = Vector3.zero + Vector3.forward * transform.position.z;
+            _camera.orthographicSize = 20f;
         }
         else
         {
             // Prevent camera's z position from changing (makes camera zoom in too close)
             transform.position = Vector3.Lerp(transform.position, _player.transform.position + Vector3.forward * transform.position.z, _lerpSpeed);
+            _camera.orthographicSize = 5f;
         }
     }
     public static void SwitchCameraZoom()
