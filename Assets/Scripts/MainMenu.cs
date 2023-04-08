dd6abbbffaa6c7289f9a601d409bb5a6eadce6ba
kevink02 <kevinkubota0@outlook.com>
2023-04-08 10:41:31 -0700

    Tweaked settings screen position

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField]
+    private GameObject _screenMain;
+    [SerializeField]
     private GameObject _screenSettings;
 
     public void SwitchSceneToLevel()
@@ -20,9 +22,14 @@ public class MainMenu : MonoBehaviour
     public void ShowScreenSettings()
     {
         _screenSettings.SetActive(true);
+        // Default screen size = 1920 x 1080 -> 1920 is length, 1920 / 2 = 960 (half of length of screen), 960 / 2 = 480 (half of half the length)
+        _screenMain.transform.localPosition = -480 * Vector2.right;
+        _screenSettings.transform.localPosition = 480 * Vector2.right;
     }
     public void HideScreenSettings()
     {
         _screenSettings.SetActive(false);
+        _screenMain.transform.localPosition = Vector2.zero;
+        _screenSettings.transform.localPosition = Vector2.zero;
     }
 }
