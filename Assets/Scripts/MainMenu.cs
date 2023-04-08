3c666fff43ba5481484c449a056077c98ed02437
kevink02 <kevinkubota0@outlook.com>
2023-04-08 10:20:26 -0700

    Allowed exit button to work

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,8 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _screenSettings;
+
     public void SwitchSceneToLevel()
     {
         SceneManager.LoadScene(sceneName: "LevelScene");
+    }
+    public void ExitGame()
+    {
+        Debug.Log($"{name}: Exiting game");
+        Application.Quit();
+    }
+    public void ShowScreenSettings()
+    {
+        _screenSettings.SetActive(true);
+    }
+    public void HideScreenSettings()
+    {
+        _screenSettings.SetActive(false);
     }
 }
