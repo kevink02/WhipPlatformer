3c1202305d1a0b75c0aa2cdca4956b57ff9456db
kevink02 <kevinkubota0@outlook.com>
2023-04-08 11:14:21 -0700

    Added pause menu script

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _screenPause;
+
+    public void SwitchSceneToMain()
+    {
+        SceneManager.LoadScene(sceneName: "MainMenu");
+    }
+    public void ShowScreenPause()
+    {
+        _screenPause.SetActive(true);
+    }
+    public void HideScreenPause()
+    {
+        _screenPause.SetActive(false);
+    }
+}
