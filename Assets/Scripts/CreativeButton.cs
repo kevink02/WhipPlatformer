326f1ee2617cfdd6906e644aa9badeb80fb23675
kevink02 <kevinkubota0@outlook.com>
2023-04-27 22:18:08 -0700

    Added custom button script

---
+++ Assets/Scripts/CreativeButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreativeButton : MonoBehaviour
+{
+    private void Awake()
+    {
+        // Allows clicking to not "hit" transparent space of image
+        Image image = GetComponent<Image>();
+        if (image)
+            image.alphaHitTestMinimumThreshold = 0.1f;
+    }
+}
