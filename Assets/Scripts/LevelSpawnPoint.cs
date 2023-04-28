846c7f435df323a36a2a1287c75ab2d858eabeb2
kevink02 <kevinkubota0@outlook.com>
2023-04-28 13:41:49 -0700

    Added level point script to objects

--- Assets/Scripts/LevelSpawnPoint.cs
+++ Assets/Scripts/LevelSpawnPoint.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class LevelSpawnPoint : MonoBehaviour
 {
+    public SpawnPointType PointType;
     public enum SpawnPointType : int
     {
         Entrance, Checkpoint, Exit
