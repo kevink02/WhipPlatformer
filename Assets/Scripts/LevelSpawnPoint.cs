503911fc85759d47d23957de8e08866c1dc3fd1b
kevink02 <kevinkubota0@outlook.com>
2023-04-28 13:04:18 -0700

    Added spawn point script

---
+++ Assets/Scripts/LevelSpawnPoint.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSpawnPoint : MonoBehaviour
+{
+    public enum SpawnPointType : int
+    {
+        Entrance, Checkpoint, Exit
+    }
+}
