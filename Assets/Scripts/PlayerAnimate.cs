16cfff98f46ba8df1f8f325630fa3539abe444e0
kevink02 <kevinkubota0@outlook.com>
2023-04-30 22:46:52 -0700

    Created player animation script

---
+++ Assets/Scripts/PlayerAnimate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimate : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    public void AbilityAttack()
+    {
+        Debug.Log($"{name}: Attacking");
+        // Prevent animation glitches by spamming attack triggers
+        if (_animator.GetBool("IsAttacking"))
+        {
+            return;
+        }
+        // Play the attack animation only once after triggering an attack
+        _animator.SetBool("IsAttacking", true);
+        StartCoroutine(EndAnimationAttack());
+    }
+    private IEnumerator EndAnimationAttack()
+    {
+        // This value should equal the speed of the attack animation in the animator window (not the length of the clip because it is NOT accurate)
+        yield return new WaitForSeconds(0.67f);
+        _animator.SetBool("IsAttacking", false);
+    }
+}
