4551b7fbc17402ad77fa826ec5d09929431ac896
kevink02 <kevinkubota0@outlook.com>
2023-05-01 15:57:51 -0700

    Updated player collider size and position

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -131,7 +131,7 @@ public class PlayerMovement : EntityMovement
         // Collider shouldn't be too big since it will interfere with the platforms in tilemaps
         // Divide by 100 since that is the sprite's pixels per unit value?
         // Divide more from x since it seems collider for player is too big (prevents them from falling down holes if walking continuously)
-        Collider.size = new Vector2(ComponentSprite.sprite.rect.size.x / 200, ComponentSprite.sprite.rect.size.y / 100);
+        Collider.size = new Vector2(ComponentSprite.sprite.rect.size.x / 135, ComponentSprite.sprite.rect.size.y / 100);
     }
     private void DoJump()
     {
