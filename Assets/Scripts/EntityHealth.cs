fb8916c599b3b8b23d6f6302fc69750773484952
kevink02 <kevinkubota0@outlook.com>
2023-05-01 00:22:31 -0700

    Created entity health class

---
+++ Assets/Scripts/EntityHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EntityHealth : MonoBehaviour, IVerification
+{
+    [SerializeField]
+    protected int Health;
+
+    private void Awake()
+    {
+        VerifyVariables();
+    }
+    public void VerifyVariables()
+    {
+        if (Health <= 0)
+        {
+            Health = 3;
+        }
+    }
+    protected abstract void TakeDamage();
+}
