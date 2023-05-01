7fb24e7684dc5e0ccf4e42cdf6e9502d6081492d
kevink02 <kevinkubota0@outlook.com>
2023-04-30 23:33:37 -0700

    Added function to enable and disable whip collider

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -7,15 +7,26 @@ public class PlayerAttack : MonoBehaviour
     [Tooltip("This value should equal the speed of the attack animation in the animator window (not the length of the clip because it is NOT accurate)")]
     public const float AnimationAttackDuration = 0.67f;
     private Animator _animator;
+    private Collider2D _collider;
 
     private void Awake()
     {
         _animator = GetComponent<Animator>();
+        _collider = GetComponent<Collider2D>();
     }
     public void AbilityAttack()
     {
         _animator.enabled = true;
+        StartCoroutine(EnableWhipCollider());
         StartCoroutine(EndAnimationAttack());
+    }
+    // Only enable whip hitbox corresponding to certain time period/frames within the attack animation
+    // Currently, only have the collider enabled when the whip is out in front of the player
+    private IEnumerator EnableWhipCollider()
+    {
+        yield return new WaitForSeconds(0.2f);
+        _collider.enabled = true;
+        yield return new WaitForSeconds(0.2f);
     }
     private IEnumerator EndAnimationAttack()
     {
