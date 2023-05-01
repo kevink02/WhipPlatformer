988cfb17b4f03c694305655bcc7700d4965150ec
kevink02 <kevinkubota0@outlook.com>
2023-04-30 20:45:58 -0700

    Added whip attack script

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log($"{name}: Collided with {collision.name}");
+    }
+}
